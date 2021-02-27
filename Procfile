d775e1ceb73e9604b80ebd03e61f58ffe75f62cb
d <d>
2021-02-27 22:39:20 +0300

    1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app_bot.py
+web: python app.py
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 TOKEN = '1619381009:AAEAlXyTON2xG6H-m2OaCAq96i63bdZyRpA'
-APP_NAME = 'https://hh-bot-wakeup.herokuapp.com/'
+APP_NAME = 'https://bot-telegram-photo-phrase.herokuapp.com/'
 
