a698e853c109d11e4a9b01f2f8dc07539fdec688
d <d>
2021-03-04 23:01:53 +0300

    1

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 TOKEN = '1617777203:AAF1P8NGG6lI0yDFtd0vn3MjkjREHUEZTuA'
-APP_NAME = 'https://telegram-bot-photo-phrase.herokuapp.com/'
+APP_NAME = 'https://bot-telegram-photo-phrase.herokuapp.com/'
 
